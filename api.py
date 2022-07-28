623d8aa7ffb2aa52d416b7a7e7aed4d58bf50b71
lucaschew717 <109940595+lucaschew717@users.noreply.github.com>
2022-07-28 17:15:13 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,23 +1,15 @@
 # 1.	Make an API call  to https://www.alphavantage.co to extract forex (FX) with the following API parameters:
 function=FX_WEEKLY
-from_symbol=USD 
-to_symbol=SGD
+from_currency=USD 
+to_currency=SGD
 # Find the mean of the weekly closing forex price from the after extracting the forex as JSON data.
 
 import requests
 url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=EUR&to_symbol=USD&apikey=TBUDCGEFDX8LB73R'
 response=requests.get(url)
-FX_WEEKLY= response.json()
-print(FX_WEEKLY)
+USD= response.json()
+print(USD)
 import csv
 from pathlib import Path
 
-fp=Path.cwd()/"FX_WEEKLY.csv"
-fp.touch()
 
-with fp.open(mode = "w", encoding="UTF-8", newline="")as file:
-    writer = csv.writer(file)
-    writer.writerow(["Date","FX_WEEKLY"])
-
-    for smthg in FX_WEEKLY(''):
-        writer.writerow([smthg["date"], smthg["value"]])
