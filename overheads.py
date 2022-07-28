4cab75e00b3fd1de7ab1120d0f547469145763b9
yaboizynex <109940464+yaboizynex@users.noreply.github.com>
2022-07-29 00:57:37 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -1,167 +1,127 @@
-import pandas as pd
+from pathlib import Path
+import re, csv
+from turtle import pen
 
-# Reading the raw data
+# Creating file path & reading the file
 
-raw_data = pd.read_csv('csv_reports/operating_expense.csv')
+path = Path(r"C:\Users\ganes\OneDrive\Desktop\project_group3_tb04\csv_reports\Overheads.csv")
+with path.open(mode="r", encoding="CP1252") as file:
+    data = file.read()
 
-# Filtering to only extract data under "Marketing Expense"
+# Extracting the data for salary expense
 
-marketing_expense_data = raw_data[raw_data['Items'] == "Marketing Expense"]
+salary_expense = (re.findall(pattern="Salary Expense.+", string=data))[0]
+salary_expense = salary_expense.split(',')
+salary_expense = salary_expense[1]
 
-# Extracting the different "Amounts" under "Marketing Expense"
+# Converting the string into a float
 
-marketing_expense_amount = marketing_expense_data[['Amount']]
+salary_expense = float(salary_expense)
 
-# Converting the data frame into a float
+# Extracting the data for interest expense
 
-marketing_expense_amount = marketing_expense_amount.astype(float)
+interest_expense = (re.findall(pattern="Interest Expense.+", string=data))[0]
+interest_expense = interest_expense.split(',')
+interest_expense = interest_expense[1]
 
-# Summimg up the different amounts
+# Converting the string into a float
 
-total_marketing_expense = marketing_expense_amount.sum()
+interest_expense = float(interest_expense)
 
+# Extracting the data for marketing expense
 
+marketing_expense = (re.findall(pattern="Marketing Expense.+", string=data))[0]
+marketing_expense = marketing_expense.split(',')
+marketing_expense = marketing_expense[1]
 
+# Converting the string into a float
 
+marketing_expense = float(marketing_expense)
 
+# Extracting the data for rental expense
 
-# Filtering to only extract data under "Salary Expense"
+rental_expense = (re.findall(pattern="Rental Expense.+", string=data))[0]
+rental_expense = rental_expense.split(',')
+rental_expense = rental_expense[1]
 
-salary_expense_data = raw_data[raw_data['Items'] == "Salary Expense"]
+# Converting the string into a float
 
-# Extracting the different "Amounts" under "Salary Expense"
+rental_expense = float(rental_expense)
 
-salary_expense_amount = salary_expense_data[['Amount']]
+# Extracting the data for overflow expense - retail
 
-# Converting the data frame into a float
+overflow_expense_retail = (re.findall(pattern="Overflow Expense - Retail.+", string=data))[0]
+overflow_expense_retail = overflow_expense_retail.split(',')
+overflow_expense_retail = overflow_expense_retail[1]
 
-salary_expense_amount = salary_expense_amount.astype(float)
+# Converting the string into a float
 
-# Summimg up the different amounts
+overflow_expense_retail = float(overflow_expense_retail)
 
-total_salary_expense = salary_expense_amount.sum()
+# Extracting the data for overflow expense - warehouse
 
+overflow_expense_warehouse = (re.findall(pattern="Overflow Expense - Warehouse.+", string=data))[0]
+overflow_expense_warehouse = overflow_expense_warehouse.split(',')
+overflow_expense_warehouse = overflow_expense_warehouse[1]
 
+# Converting the string into a float
 
+overflow_expense_warehouse = float(overflow_expense_warehouse)
 
+# Extracting the data for penalty expense
 
+penalty_expense = (re.findall(pattern="Penalty Expense.+", string=data))[0]
+penalty_expense = penalty_expense.split(',')
+penalty_expense = penalty_expense[1]
 
-# Filtering to only extract data under "Rental Expense"
+# Converting the string into a float
 
-rental_expense_data = raw_data[raw_data['Items'] == "Rental Expense"]
+penalty_expense = float(penalty_expense)
 
-# Extracting the different "Amounts" under "Rental Expense"
+# Extracting the data for shipping expense
 
-rental_expense_amount = rental_expense_data[['Amount']]
+shipping_expense = (re.findall(pattern="Shipping Expense.+", string=data))[0]
+shipping_expense = shipping_expense.split(',')
+shipping_expense = shipping_expense[1]
 
-# Converting the data frame into a float
+# Converting the string into a float
 
-rental_expense_amount = rental_expense_amount.astype(float)
+shipping_expense = float(shipping_expense)
 
-# Summimg up the different amounts
+# Extracting the data for human resource expense
 
-total_rental_expense = rental_expense_amount.sum()
+human_resource_expense = (re.findall(pattern="Human Resource Expense.+", string=data))[0]
+human_resource_expense = human_resource_expense.split(',')
+human_resource_expense = human_resource_expense[1]
 
+# Converting the string into a float
 
+human_resource_expense = float(human_resource_expense)
 
+# Loop to find out which overhead expense is the highest
 
+if salary_expense>interest_expense and salary_expense>marketing_expense and salary_expense>rental_expense and salary_expense>overflow_expense_retail and salary_expense>overflow_expense_warehouse and salary_expense>penalty_expense and salary_expense>shipping_expense and salary_expense>human_resource_expense:
+    print(f"Salary expenses are the highest overhead expense at {salary_expense}")
 
+elif interest_expense>salary_expense and interest_expense>marketing_expense and interest_expense>rental_expense and interest_expense>overflow_expense_retail and interest_expense>overflow_expense_warehouse and interest_expense>penalty_expense and interest_expense>shipping_expense and interest_expense>human_resource_expense:
+    print (f"Interest expenses are the highest overhead expense at {interest_expense}")
 
-# Filtering to only extract data under "Penalty Expense"
+elif marketing_expense>salary_expense and marketing_expense>interest_expense and marketing_expense>rental_expense and marketing_expense>overflow_expense_retail and marketing_expense>overflow_expense_warehouse and marketing_expense>penalty_expense and marketing_expense>shipping_expense and marketing_expense>human_resource_expense:
+    print (f"Marketing expenses are the highest overhead expense at {marketing_expense}")
 
-penalty_expense_data = raw_data[raw_data['Items'] == "Penalty Expense"]
+elif rental_expense>salary_expense and rental_expense>interest_expense and rental_expense>marketing_expense and rental_expense>overflow_expense_retail and rental_expense>overflow_expense_warehouse and rental_expense>penalty_expense and rental_expense>shipping_expense and rental_expense>human_resource_expense:
+    print (f"Rental expenses are the highest overhead expense at {rental_expense}")
 
-# Extracting the different "Amounts" under "Penalty Expense"
+elif overflow_expense_retail>salary_expense and overflow_expense_retail>interest_expense and overflow_expense_retail>marketing_expense and overflow_expense_retail>rental_expense and overflow_expense_retail>overflow_expense_warehouse and overflow_expense_retail>penalty_expense and overflow_expense_retail>shipping_expense and overflow_expense_retail>human_resource_expense:
+    print (f"Overflow Expense - Retail is the highest overhead expense at {overflow_expense_retail}")
 
-penalty_expense_amount = penalty_expense_data[['Amount']]
+elif overflow_expense_warehouse>salary_expense and overflow_expense_warehouse>interest_expense and overflow_expense_warehouse>marketing_expense and overflow_expense_warehouse>rental_expense and overflow_expense_warehouse>overflow_expense_retail and overflow_expense_warehouse>penalty_expense and overflow_expense_warehouse>shipping_expense and overflow_expense_warehouse>human_resource_expense:
+    print (f"Overflow Expense - Warehouse is the highest overhead expense at {overflow_expense_warehouse}")
 
-# Converting the data frame into a float
-
-penalty_expense_amount = penalty_expense_amount.astype(float)
-
-# Summimg up the different amounts
-
-total_penalty_expense = penalty_expense_amount.sum()
-
-
-
-
-
-
-# Filtering to only extract data under "Shipping Expense"
-
-shipping_expense_data = raw_data[raw_data['Items'] == "Shipping Expense"]
-
-# Extracting the different "Amounts" under "Shipping Expense"
-
-shipping_expense_amount = shipping_expense_data[['Amount']]
-
-# Converting the data frame into a float
-
-shipping_expense_amount = shipping_expense_amount.astype(float)
-
-# Summimg up the different amounts
-
-total_shipping_expense = shipping_expense_amount.sum()
-
-
-
-
-
-
-# Filtering to only extract data under "Overflow Expense - Warehouse"
-
-shipping_expense_data = raw_data[raw_data['Items'] == "Overflow Expense - Warehouse"]
-
-# Extracting the different "Amounts" under "Overflow Expense - Warehouse"
-
-warehouse_expense_amount = shipping_expense_data[['Amount']]
-
-# Converting the data frame into a float
-
-warehouse_expense_amount = warehouse_expense_amount.astype(float)
-
-# Summimg up the different amounts
-
-total_warehouse_expense = warehouse_expense_amount.sum()
-
-
-
-
-
-
-# Filtering to only extract data under "Overflow Expense - Retail"
-
-retail_expense_data = raw_data[raw_data['Items'] == "Overflow Expense - Retail"]
-
-# Extracting the different "Amounts" under "Overflow Expense - Retail"
-
-retail_expense_amount = retail_expense_data[['Amount']]
-
-# Converting into a float
-
-retail_expense_amount = retail_expense_amount.astype(float)
-
-# Summimg up the different amounts
-
-total_retail_expense = retail_expense_amount.sum()
-
-
-
-
-
-# Filtering to only extract data under "Human Resource Expense"
-
-hr_expense_data = raw_data[raw_data['Items'] == "Human Resource Expense"]
-
-# Extracting the different "Amounts" under "Overflow Expense - Retail"
-
-hr_expense_amount = hr_expense_data[['Amount']]
-
-# Converting into a float
-
-hr_expense_amount = hr_expense_amount.astype(float)
-
-# Summimg up the different amounts
-
-total_hr_expense = hr_expense_amount.sum()
+elif penalty_expense>salary_expense and penalty_expense>interest_expense and penalty_expense>marketing_expense and penalty_expense>rental_expense and penalty_expense>overflow_expense_retail and penalty_expense>overflow_expense_warehouse and penalty_expense>shipping_expense and penalty_expense>human_resource_expense:
+    print (f"Penalty expenses are the highest overhead expense at {penalty_expense}")
+    
+elif shipping_expense>salary_expense and shipping_expense>interest_expense and shipping_expense>marketing_expense and shipping_expense>rental_expense and shipping_expense>overflow_expense_retail and shipping_expense>overflow_expense_warehouse and shipping_expense>penalty_expense and shipping_expense>human_resource_expense:
+    print (f"Shipping expenses are the highest overhead expense at {shipping_expense}")
+elif human_resource_expense>salary_expense and human_resource_expense>interest_expense and human_resource_expense>marketing_expense and human_resource_expense>rental_expense and human_resource_expense>overflow_expense_retail and human_resource_expense>overflow_expense_warehouse and human_resource_expense>penalty_expense and human_resource_expense>shipping_expense:
+    print (f"Human resource expenses are the highest overhead expense at {human_resource_expense}")
