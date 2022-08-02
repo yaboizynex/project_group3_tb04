7b1ce4e50232031e7ed5ae13a657b5f4b01dfa40
S10247525 <109942177+S10247525@users.noreply.github.com>
2022-08-02 19:53:21 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,60 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# programm to calc if the cash on hand on the following day is less than the previous day
+from pathlib import Path
+import csv
 
-import pandas as pd 
+file_path= Path.cwd()/"Cash on Hand.csv"
 
-def coh_function(forex):
-    #read input data
-    df = pd.read_csv('csv_reports/Cash_on_Hand.csv')
-    
-    #extract 2 columns from 
-    df = df[['Day', "Amount"]]
-    #print(df.head())
-    #print()
-    
-    
-    #calculate the unique ordered days
-    days=set(df['Day'])
-    
-    #calculate sums of amount for each day
-    data=df.groupby(['Day']).sum()
-    
-    #print(data)
-    #print()
-    
-    df=data
-    
-    #how many row (how many days) we have
-    rows_count = len(df.index)
-    
-    #array with previous  day's summarised amount
-    prev=df['Amount'][0:rows_count-1].to_numpy()
-    
-    #array with next day's summarised amount
-    next=df['Amount'][1:rows_count].to_numpy()
-    
-    #difference
-    diff=next - prev
-    #print(diff)
-    
-    prev=df['Amount'][0:rows_count].to_numpy()
-    
-    #convert to list
-    days=list(days)
-    #print(days[0],': NaN')
-    #for i in range(1,len(days)):
-        #print(days[i],': ', diff[i-1], '  prev: ', prev[i])
-    
-    #compare summarised amount for each day
-    flag=False; #not found
-    for i in range(0,len(days)):
-        try:
-            if diff[i]<0:
-                flag=True #found
-                print('[CASH DEFICIT] DAY: %.1f, AMOUNT: %.1f' % (days[i+1], prev[i+1]))
-        except:
-            continue
-    
-    
-    if not flag:
-        print('CASH OF EACH DAYS IS HIGHER THAN THE PREVIOUS DAY')
+with open('Cash on Hand.csv') as csv_file:
+    triggered = False  # to keep track of if any this happens even once
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    previous_cash = 0  # keep track of the cash on the previous day
+    for row in csv_reader:
+        if line_count == 0:   # to skip the first line of headers
+          pass
+        else:
+            day, cash = int(row[0]), int(row[1])  # getting the day and the cash
+            if ((cash - previous_cash) < 0):
+                triggered = True
+                print(
+                    f"Cash on hand, on day {day} is lower than that on the previous day by {previous_cash - cash}")
+            previous_cash = cash
+        line_count += 1
+
+if(not triggered):
+  print("The cash on hand on each day was more than to previous day, no days to highlight.")
