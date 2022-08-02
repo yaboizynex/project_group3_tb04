cfcea2b883e81937f61c4938207f303852a67a90
lucaschew717 <109940595+lucaschew717@users.noreply.github.com>
2022-08-02 21:39:10 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,22 @@
-# 1.	Make an API call  to https://www.alphavantage.co to extract forex (FX) with the following API parameters:
-function=CURRENCY_EXCHANGE_RATE
-from_currency=USD 
-to_currency=SGD
-# Find the mean of the weekly closing forex price from the after extracting the forex as JSON data.
+# function=CURRENCY_EXHANGE_RATE
+# from_currency=USD 
+# to_currency=SGD
 
-import requests
-url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=EUR&to_symbol=USD&apikey=TBUDCGEFDX8LB73R'
-response=requests.get(url)
-USD= response.json()
-print(USD)
-import csv
-from pathlib import Path
+
+from fileinput import close
+import re,requests
+import json
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=TBUDCGEFDX8LB73R'
+r = requests.get(url)
+data = r.json()
+#dict_keys(['Realtime Currency Exchange Rate'])
+
+
+for i in data.values():
+    print(i)
+
+print(json.dumps(data['Realtime Currency Exchange Rate'], indent=4))
+for file in data:
+    USD=re.findall(pattern="[Exchange Rate]", string=data)
 
 
