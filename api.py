4aec1f73104db2f798857a4e70a842f128cfadb4
S10247525 <109942177+S10247525@users.noreply.github.com>
2022-08-03 23:49:34 +0800

    Update api.py

--- api.py
+++ api.py
@@ -23,3 +23,28 @@ def CURRENCY_EXHANGE_RATE():
    fp=Path.cwd()/"summary.text"
    fp.touch()
    with fp.open(mode = "w", encoding="UTF-8", newline="")as file:
+
+    
+    
+################################
+import requests
+api_key = "TBUDCGEFDX8LB73R"
+url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+response= requests.get(url)
+
+# # print(response.json())
+exchange_rate= response.json()
+# import json
+# #print(json.dumps(inflation, indent=4))
+# for eachIDD in exchange_rate ["data"]4
+# print(eachIDD['value'])
+import csv
+from pathlib import Path
+
+fp_write=Path.cwd()/"summary.txt"
+fp_write.touch()
+with fp_write.open(mode= "w", encoding="utf-8", newline="") as file:
+    writer= csv.writer(file)
+    writer.writerow(["Date" ,"exchange rate"])
+    for eachIDD in exchange_rate [""]:
+        writer.writerow([eachIDD["date"],eachIDD["value"]
