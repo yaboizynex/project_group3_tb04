1d0da601f58f9f151959405a1d984388b81ace33
JUNHAO69 <109966134+JUNHAO69@users.noreply.github.com>
2022-08-04 15:17:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+import api,coh,overheads,profit_loss
+def main():
+  forex =api.api_function()
+  overheads.overhead_function(forex)
+  coh.coh_function(forex)
+  profit_loss.profitloss_function(forex)
+  
+  main()
