0098d5147714650f927e6bfde1748eae7bba1008
S10247525 <109942177+S10247525@users.noreply.github.com>
2022-08-04 23:11:10 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,32 +1,4 @@
- function=CURRENCY_EXHANGE_RATE
-# from_currency=USD 
-# to_currency=SGD
 
-
-from fileinput import close
-import csv
-from pathlib import Path
-import re,requests
-import json
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=TBUDCGEFDX8LB73R'
-r = requests.get(url)
-data = r.json()
-#dict_keys(['Realtime Currency Exchange Rate'])
-
-
-for i in data.values():
-    print(i)
-
-x=json.dumps(data['Realtime Currency Exchange Rate'], indent=4)
-print(x)
-def CURRENCY_EXHANGE_RATE():
-   fp=Path.cwd()/"summary.text"
-   fp.touch()
-   with fp.open(mode = "w", encoding="UTF-8", newline="")as file:
-
-    
-    
-################################
 import requests
 api_key = "TBUDCGEFDX8LB73R"
 url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
@@ -46,9 +18,9 @@ fp_write.touch()
 with fp_write.open(mode= "w", encoding="utf-8", newline="") as file:
     writer= csv.writer(file)
     writer.writerow(["[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD1.38537"])
-    writer.writerow(["[HIGHEST OVERHEADS] SALARY EXPENSE: SGD39.9"])
-    writer.writerow(["[CASH SURPLUS]"])
-    writer.writerow(["[NET PROFIT SURPLUS]"])
+    writer.writerow(["[HIGHEST OVERHEADS] RENTAL EXPENSE: SGD33.03"])
+    writer.writerow(["[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"])
+    writer.writerow(["[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"])
     for eachIDD in exchange_rate [""]:
         writer.writerow(eachIDD["date"],eachIDD["value"])
 
