b238242cc4b7463ade0e60ddaa3a08448c18ac6d
JUNHAO69 <109966134+JUNHAO69@users.noreply.github.com>
2022-08-04 15:19:05 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import csv
 
-file_path= Path.cwd()/"Cashon Hand.csv"
+file_path= Path.cwd()/"CashonHand.csv"
 
 with open('Cash on Hand.csv') as csv_file:
     triggered = False  # to keep track of if any this happens even once
